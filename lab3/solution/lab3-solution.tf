5c225714a6c72f0693cb5d9c93831b2a0b495c24
Harshal Dharia <hadharia@microsoft.com>
2018-10-10 20:22:24 +0000

    Updated output for solution

--- lab3/solution/lab3-solution.tf
+++ lab3/solution/lab3-solution.tf
@@ -100,10 +100,6 @@ output "app_service_default_hostname" {
   value = "https://${azurerm_app_service.asp-webapp.default_site_hostname}"
 }
 
-output "app_service_dev_default_hostname" {
-  value = "https://${azurerm_app_service_slot.asp-webapp.default_site_hostname}"
-}
-
 output "connection_string" {
   description = "Connection string for the Azure SQL Database created."
   value       = "Server=tcp:${azurerm_sql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${azurerm_sql_server.server.administrator_login};Password=${azurerm_sql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
