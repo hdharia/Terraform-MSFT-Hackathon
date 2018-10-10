d042ccc113c128949cab1b5c720319ec1783864a
Harshal Dharia <hadharia@microsoft.com>
2018-10-10 20:21:14 +0000

    Added exercise tags

--- lab3/lab3.tf
+++ lab3/lab3.tf
@@ -13,6 +13,7 @@ resource "azurerm_resource_group" "asp-webapp"
   location = "eastus"
 }
 
+#Exerice 1
 resource "azurerm_app_service_plan" "asp-webapp"
 {
   name = "asp-webapp-plan"
@@ -26,27 +27,44 @@ resource "azurerm_app_service_plan" "asp-webapp"
   }
 }
 
+#Exericse 2
 /*
 resource "azurerm_app_service" "asp-webapp" {
   
 }
 */
 
+#Exercise 3
 /*
 resource "azurerm_app_service_slot" "asp-webapp" {
 
 }
 */
 
+#Exercise 4
 /*
-output "app_service_default_hostname" {
+resource "azurerm_sql_server" "server" {
+
+}
+*/
+
+#Exercise 4
+/*
+resource "azurerm_sql_database" "db" {
   
 }
 */
 
-/*
-output "app_service_dev_default_hostname" {
-  
+/* #Exerice 3
+output "app_service_default_hostname" {
+  value = "https://${azurerm_app_service.asp-webapp.default_site_hostname}"
+}
+*/
+
+/* #Exercise 4
+output "connection_string" {
+  description = "Connection string for the Azure SQL Database created."
+  value       = "Server=tcp:${azurerm_sql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${azurerm_sql_server.server.administrator_login};Password=${azurerm_sql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 */
 
