09c2fceb871f028c2ce475e8c20007e6716fb135
Harshal Dharia <hadharia@microsoft.com>
2018-10-16 14:55:53 +0000

    Updated solutions

--- lab3/solution/variables.tf
+++ lab3/solution/variables.tf
@@ -1,5 +1,5 @@
- variable "subscription_id" {default = "7d7d2a39-4806-458d-9d2a-2944e1fa7502"} 
- variable "client_id" {default = "69bb851b-b2f4-4ad5-8886-a541d68c5f28"}
- variable "client_secret" {default = "Q0dzlzp6vNqc0O7tLoXxU6x38iylVCo8MXMTYEFqV7w="}
- variable "tenant_id" {default = "72f988bf-86f1-41af-91ab-2d7cd011db47"}
- variable "environment" {default = "public"}
+ variable "subscription_id" {default = ""} 
+ variable "client_id" {default = ""}
+ variable "client_secret" {default = ""}
+ variable "tenant_id" {default = ""}
+ variable "environment" {default = ""}
